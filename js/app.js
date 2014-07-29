e9351ea9911f7519950e5c4d6cc12a3b5a4f30e7
efinkelstein25 <estherfinkelstein@Esthers-MacBook-Air.local>
2014-07-28 21:29:16 -0400

    Added randomize function and prelim Hot/Cold test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function(){
+		
+    var random;
+
+    random = newGame();
+
+    $(".new").on('click', function(){
+     random = newGame();
+    });
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +20,50 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
+  	$('#guessButton').on('click', function(e){
+      e.preventDefault();
+      var guessValue = $('#userGuess').val();
+      HotOrCold(random, guessValue);
+    });
+
 });
 
+
+/*starts a new game*/
+function newGame(){
+/*have other stuff here*/
+
+var randomNum = randomize();
+console.log(randomNum);
+
+return randomNum;
+
+
+}
+
+/*creates a random number between 1 and 100*/
+function randomize(){
+	var randomNum = Math.floor((Math.random() * 100) + 1);
+	return randomNum;
+}
+
+/*takes in user value, sees if it is hot or cold compared to random number*/
+function HotOrCold(random, input){
+
+var highestHot, lowestHot;
+
+highestHot = random + 20;
+lowestHot = random - 20;
+
+if (input >= lowestHot && input <= highestHot)
+{
+  console.log("It's hot!");
+}
+else{
+  console.log("It's cold!");
+}
+
+}
 
